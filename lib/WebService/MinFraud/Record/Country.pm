cab5d4b85001905ba58b75ec1064695fb634626e
Gregory Oschwald <goschwald@maxmind.com>
2018-01-17 15:35:10 -0800

    Update to GeoIP2 2.005000
    This adds is_in_european_union support.

--- lib/WebService/MinFraud/Record/Country.pm
+++ lib/WebService/MinFraud/Record/Country.pm
@@ -5,7 +5,7 @@ use namespace::autoclean;
 
 our $VERSION = '1.005002';
 
-use GeoIP2::Record::Country 2.004000;
+use GeoIP2::Record::Country 2.005000;
 use WebService::MinFraud::Types qw( Bool BoolCoercion );
 
 extends 'GeoIP2::Record::Country';
